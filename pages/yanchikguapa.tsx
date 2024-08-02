6038a09ed6bf8fc46992e10c49644a9fd2803aac
juancarlossa <juank987610@gmail.com>
2024-08-02 15:52:56 +0200

    Updated Counter 02.08

--- pages/yanchikguapa.tsx
+++ pages/yanchikguapa.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import { useEffect, useState } from 'react';
 
 export default function Counter () {
@@ -21,7 +21,7 @@ export default function Counter () {
     }, []);
 
     function calculateTimeRemaining (): number {
-      const targetDate = new Date('2024-06-02T14:00:00').getTime();
+      const targetDate = new Date('2024-08-22T14:21:00').getTime();
       const now = new Date().getTime();
       return Math.max(targetDate - now, 0);
     }
@@ -32,13 +32,12 @@ export default function Counter () {
     );
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
     return (
       <div className="text-2xl w-full mt-4 font-semibold text-gray-800 bg-gradient-to-r from-cyan-300 to-purple-500 transition duration-1000 p-4 rounded-lg shadow-lg">
         <h1 className="text-4xl sm:text-md md:text-4xl py-4 text-center pt-10 text-white font-medium ">🍀 Янчик 👫 Хуанчик 🍀</h1>
         <h2 className='text-7xl sm:text-md md:text-md text-black text-center p-10 font-bold pb-10'>{days}д : {hours}ч : {minutes}м : {seconds}с</h2>
-        <h3 className='text-center text-blue-800 font-extralight italic'>Aeropuerto de Barajas</h3>
-        <h3 className='text-center text-blue-800 pb-10 font-extralight italic'>Madrid, Españita</h3>
+        <h3 className='text-center text-blue-800 font-extralight italic'>Международный аэропорт Внуково</h3>
+        <h3 className='text-center text-blue-800 pb-10 font-extralight italic'>Москва, Россия</h3>
       </div>
     );
   };
